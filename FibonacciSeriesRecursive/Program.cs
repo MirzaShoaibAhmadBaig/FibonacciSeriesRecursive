2d7c8b525969d0d7f7b0266a0a2e5c17264937e7
armanahmd <92032906+armanahmd@users.noreply.github.com>
2022-03-20 22:18:11 +0500

    Add project files.

---
+++ FibonacciSeriesRecursive/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FibonacciSeriesRecursive
+{
+    class Program
+    {
+        static int Firstnum = 0, Secondnum = 1, Final;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your Number");
+            int UserNum = Int32.Parse(Console.ReadLine());
+            FibonacciRecursive(UserNum);
+
+        }
+
+        public static void FibonacciRecursive(int UserNum)
+        {
+            if(UserNum>0)
+            {
+                Final = Firstnum + Secondnum;
+                Console.WriteLine(Final);
+                Firstnum = Secondnum;
+                Secondnum = Final;
+                FibonacciRecursive(UserNum - 1);
+
+
+
+            }
+
+
+
+
+        }
+    }
+}
